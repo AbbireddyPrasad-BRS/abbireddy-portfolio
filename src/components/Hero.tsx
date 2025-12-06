b969c46a0ecc054c8b351bfaa15b466accab8663
A V VENKATA SATYA SAI PRASAD <141221857+AbbireddyPrasad-BRS@users.noreply.github.com>
2025-12-06 16:56:05 +0530

    Update Google Drive link in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -165,7 +165,7 @@ const Hero = () => {
             <Code2 className="h-6 w-6 sm:h-7 sm:w-7 md:h-8 md:w-8" />
           </a>
           <a
-            href="https://drive.google.com/file/d/1yEkz-T44DP_KHGjw9F_atUxoBceW_ukJ/view?usp=sharing"
+            href="https://drive.google.com/file/d/11k2zsqQM9_SF1YxVcJBZETW6IrnFgnqN/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="text-muted-foreground hover:text-cyber-cyan transition-colors duration-300 transform hover:scale-110"
